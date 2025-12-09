34a56fe2bb12df03e55074b09afdfd2ec83dacfc
ahryeong616 <yanyaling2540@gmail.com>
2025-12-09 10:50:29 +0900

    InteractiveComicPanel

---
+++ components/InteractiveComicPanel.tsx
@@ -0,0 +1,11 @@
+export function InteractiveComicPanel() {
+    return <div>Interactive Comic Panel</div>;
+}
+
+export function drawTextOnImage() {
+    return null;
+}
+
+export function getDefaultBubbleConfig() {
+    return {};
+}

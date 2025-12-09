7e88002bc664ac647fa6dd3ba0069e75e53a36cf
ahryeong616 <yanyaling2540@gmail.com>
2025-12-09 10:48:45 +0900

    ComicPanel

---
+++ components/ComicPanel.tsx
@@ -0,0 +1,7 @@
+export default function ComicPanel({ children }) {
+    return (
+        <div style={{ border: "1px solid #ddd", padding: "10px" }}>
+            {children}
+        </div>
+    );
+}
